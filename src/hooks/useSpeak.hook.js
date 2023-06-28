19dc6462acc75081b8bd7538d3b042edc95689be
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-06-28 16:42:25 -0300

    delete code from old implementations of useRecord and useSpeak

--- src/hooks/useSpeak.hook.js
+++ src/hooks/useSpeak.hook.js
@@ -1,30 +1,5 @@
 import { useRef, useState } from "react";
-import { useVoice } from "./useVoice.hook";
-import { speak } from "../utils/functions/speak";
 import { Speaker } from "../utils/classes/Speaker";
-
-// export const useSpeak = () => {
-//     const voice = useVoice();
-
-//     const cancelSpeaking = useRef(() => {});
-//     const [isSpeaking, setIsSpeaking] = useState(false);
-//     const startSpeaking = () => setIsSpeaking(true);
-//     const stopSpeaking = () => {
-//         cancelSpeaking.current();
-//         setIsSpeaking(false);
-//     };
-
-//     const speakFunc = (text) => {
-//         startSpeaking();
-//         cancelSpeaking.current = speak(text, voice, stopSpeaking);
-//     };
-
-//     return {
-//         speak: speakFunc,
-//         stopSpeaking,
-//         isSpeaking: isSpeaking,
-//     };
-// };
 
 export const useSpeak = () => {
     const [isSpeaking, setIsSpeaking] = useState(false);
