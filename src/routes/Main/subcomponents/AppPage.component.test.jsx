4c97659220829b48059d700de8f685a834fa9d88
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-07-24 15:28:43 -0300

    fix api address

---
+++ src/routes/Main/subcomponents/AppPage.component.test.jsx
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import { AppPage } from "./AppPage.component";
+
+describe("AppPage", () => {
+    it("should render without errors", async () => {});
+});
