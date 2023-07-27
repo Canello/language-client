6227aca6cf3cfa311883da94adce41276553661f
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-07-27 12:22:00 -0300

    add test for ContactModal

---
+++ src/components/Footer/subcomponents/ContactModal.component.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { ContactModal } from "./ContactModal.component";
+
+jest.mock("react-dom", () => {
+    return {
+        ...jest.requireActual("react-dom"),
+        createPortal: (el) => el,
+    };
+});
+
+describe("ContactModal", () => {
+    it("should render without errors", async () => {
+        render(<ContactModal isShowing={true} onClose={() => {}} />);
+
+        const contactModalElement = screen.getByTestId("ContactModalStyled");
+        expect(contactModalElement).toBeInTheDocument();
+    });
+});
