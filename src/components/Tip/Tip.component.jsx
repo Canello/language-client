8653a4c9e41208f16d8cf99035a3d0c5e2a22af0
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-07-28 16:06:59 -0300

    minor

--- src/components/Tip/Tip.component.jsx
+++ src/components/Tip/Tip.component.jsx
@@ -2,9 +2,5 @@ import { TipStyled } from "./Tip.styles";
 
 export const Tip = ({ children, isShowing = true, ...props }) => {
     if (!isShowing) return null;
-    return (
-        <TipStyled isShowing={isShowing} {...props}>
-            {children}
-        </TipStyled>
-    );
+    return <TipStyled {...props}>{children}</TipStyled>;
 };
